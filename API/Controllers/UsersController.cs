974b840c9702ad0477340f7c155480814d2ddc82
therealkundanpatil <kp28021996@gmail.com>
2021-09-19 19:59:51 -0400

    Commit Message
    + Hashed password so that it can be stored in the database
    + Made use of Data Transfer objects and JSON web tokens to implement
      basic authentication and validation.
    + Created and used service for our application

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -5,12 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         public UsersController(DataContext context)
@@ -19,11 +18,13 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
             return await _context.Users.ToListAsync();
         }
 
         // api/users/3
+        [Authorize]
         [HttpGet("{id}")]
         public ActionResult<AppUser> GetUser(int id){
             return  _context.Users.Find(id);
