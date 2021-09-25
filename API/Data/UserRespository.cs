8942ebee96febf9eedbecbfeb804470c53bbdb69
therealkundanpatil <kp28021996@gmail.com>
2021-09-25 18:06:01 -0400

    Commit Message
    + Implemented pagination on both server side and client side
    + Implemented filtering on both server side and client side
    + Implemented sorting on both server side and client side
    + Formatted Date and leveraged ngx-timeago library
    + Implemented caching in client for paginated resources

--- API/Data/UserRespository.cs
+++ API/Data/UserRespository.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -22,9 +24,24 @@ namespace API.Data
             _mapper = mapper;
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMemberAsync()
+        public async Task<PagedList<MemberDto>> GetMemberAsync(UserParams userParams)
         {
-            return await _context.Users.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).ToListAsync();
+            var query = _context.Users.AsQueryable();
+
+            query = query.Where(u => u.UserName != userParams.CurrentUsername);
+            query = query.Where(u => u.Gender == userParams.Gender);
+
+            var minDob = DateTime.Today.AddYears(-userParams.MaxAge - 1);
+            var maxDob = DateTime.Today.AddYears(-userParams.MinAge);
+
+            query = query.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            
+            query = userParams.OrderBy switch{
+                "created" => query.OrderByDescending(u => u.Created),
+                _ => query.OrderByDescending(u => u.LastActive)
+            };
+
+            return await PagedList<MemberDto>.CreateAsync(query.ProjectTo<MemberDto>(_mapper.ConfigurationProvider).AsNoTracking(), userParams.PageNumber,userParams.PageSize);
         }
 
         public async Task<IEnumerable<AppUser>> GetUserAsync()
--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 
 namespace API.Interfaces
 {
@@ -17,7 +18,7 @@ namespace API.Interfaces
 
         Task<AppUser> GetUserByUserNameAsync(string username);
 
-        Task<IEnumerable<MemberDto>> GetMemberAsync();
+        Task<PagedList<MemberDto>> GetMemberAsync(UserParams userParams);
 
         Task<MemberDto> GetMemberAsync(string username);
     }
