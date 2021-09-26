e8706b599802e4b050d9ade54d084d5f1ebc4c76
therealkundanpatil <kp28021996@gmail.com>
2021-09-25 20:23:57 -0400

    Commit Message
    + Implemented many to many likes relationship using joint table
    + Configured entities in the DbContext

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -14,6 +14,7 @@ namespace API.Extensions
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IPhotoService,PhotoService>();
+            services.AddScoped<ILikesRepository, LikesRepository>();
             services.AddScoped<LogUserActivity>();
             services.AddScoped<IUserRepository, UserRespository>();
             services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
